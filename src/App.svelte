fa9e8625bb611ce6e237828f5394f6f3f6b16b3d
Kynson <46522440+Kynson@users.noreply.github.com>
2021-05-21 22:24:30 +0800

    Update project to use svelte

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<!-- Logic for App -->
+<script lang="ts">
+  import { Router } from '@roxi/routify';
+  import { routes } from '../.routify/routes';
+
+  import TailwindCSS from '@components/TailwindCSS.svelte';
+</script>
+
+<!-- Inject global style here -->
+<TailwindCSS />
+
+<!-- Style for App -->
+<style lang="scss">
+  @import "./App.scss";
+</style>
+
+<Router {routes} />
