a5e47b905c0367002fb12dd23ad74c17d44b4e60
Kynson Szetau <46522440+Kynson@users.noreply.github.com>
2025-01-01 17:03:26 +0800

    Add turnstile.ts

---
+++ src/scripts/turnstile.ts
@@ -0,0 +1,34 @@
+// Creating side effects in modules are not recommended.
+// Therefore, we wrap the initialization code inside a function
+export default function initializeTurnstile() {
+  const turnstileSpinner = document.querySelector('#turnstile-spinner')!;
+  const turnstileStatusCheckbox = document.querySelector(
+    '#turnstile-status-checkbox',
+  )! as HTMLInputElement;
+
+  // Inject turnstile script
+  const scriptElement = document.createElement('script');
+  scriptElement.src =
+    'https://challenges.cloudflare.com/turnstile/v0/api.js?render=explicit&onload=turnstileLoadHandler';
+  document.head.appendChild(scriptElement);
+
+  function markTurnstileVerified() {
+    turnstileStatusCheckbox.checked = true;
+  }
+
+  function resetTurnstileStatus() {
+    turnstileStatusCheckbox.checked = false;
+  }
+
+  window.turnstileLoadHandler = () => {
+    turnstileSpinner.classList.add('hidden');
+
+    window.turnstile.render('#turnstile-widget', {
+      sitekey: '3x00000000000000000000FF',
+      callback: markTurnstileVerified,
+      'expired-callback': resetTurnstileStatus,
+      'error-callback': resetTurnstileStatus,
+      theme: 'dark',
+    });
+  };
+}
