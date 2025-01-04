93183c49d86b95a4270e08cab56cb774a61fe5d0
Kynson Szetau <46522440+Kynson@users.noreply.github.com>
2025-01-04 16:00:32 +0800

    Implement custom octokit

---
+++ content-sync/src/octokit.ts
@@ -0,0 +1,74 @@
+import type {
+  EmitterWebhookEvent,
+  EmitterWebhookEventName,
+} from '@octokit/webhooks';
+
+import { createAppAuth } from '@octokit/auth-app';
+import { request } from '@octokit/request';
+import { Webhooks } from '@octokit/webhooks';
+
+interface OctokitConstructOptions {
+  appID: string;
+  privateKey: string;
+  webhookSecret: string;
+  installationID: string;
+}
+
+type HandlerFunction<K extends EmitterWebhookEventName> = (
+  event: EmitterWebhookEvent<K>
+) => any;
+
+interface OctokitInitializeOptions<K extends EmitterWebhookEventName>
+  extends OctokitConstructOptions {
+  webhookHandlerMappings: Record<K, HandlerFunction<K>>;
+}
+
+class Octokit {
+  // The type of request is not exported
+  request: typeof request;
+  webhooks: Webhooks;
+
+  constructor({
+    appID,
+    privateKey,
+    webhookSecret,
+    installationID,
+  }: OctokitConstructOptions) {
+    const auth = createAppAuth({
+      appId: appID,
+      privateKey,
+      installationId: installationID,
+    });
+
+    this.request = request.defaults({
+      request: {
+        hook: auth.hook,
+      },
+    });
+
+    this.webhooks = new Webhooks({
+      secret: webhookSecret,
+    });
+  }
+}
+
+export let octokit: Octokit | null = null;
+
+export function initializeOctokit<K extends EmitterWebhookEventName>({
+  webhookHandlerMappings,
+  ...constructOptions
+}: OctokitInitializeOptions<K>) {
+  if (octokit) {
+    return octokit;
+  }
+
+  octokit = new Octokit(constructOptions);
+
+  for (const [webhookName, handler] of Object.entries<HandlerFunction<K>>(
+    webhookHandlerMappings
+  )) {
+    octokit.webhooks.on(webhookName as K, handler);
+  }
+
+  return octokit;
+}
