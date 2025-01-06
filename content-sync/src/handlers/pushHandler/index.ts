78874ac70f8757587cce878b5ef955a4aaad4dc0
Kynson Szetau <46522440+Kynson@users.noreply.github.com>
2025-01-06 12:32:18 +0800

    Optimize pushHandler.ts

--- content-sync/src/handlers/pushHandler/index.ts
+++ content-sync/src/handlers/pushHandler/index.ts
@@ -6,13 +6,13 @@ export default async function pushHandler(
   { repository }: EmitterWebhookEventPayload<'push'>,
   environment: Env
 ) {
-  const aboutResponse = await fetch(
-    'https://raw.githubusercontent.com/Kynson/Kynson/main/README.md'
-  );
-
   if (repository.full_name !== 'Kynson/Kynson') {
     return;
   }
+
+  const aboutResponse = await fetch(
+    'https://raw.githubusercontent.com/Kynson/Kynson/main/README.md'
+  );
 
   const aboutContent = await parseAboutResponse(aboutResponse);
 
