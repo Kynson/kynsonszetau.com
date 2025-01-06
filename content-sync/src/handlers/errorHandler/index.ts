168d16de34fe803b05775e3b48489d535246616b
Kynson Szetau <46522440+Kynson@users.noreply.github.com>
2025-01-06 13:10:02 +0800

    Improve logging

--- content-sync/src/handlers/errorHandler/index.ts
+++ content-sync/src/handlers/errorHandler/index.ts
@@ -1,15 +1,21 @@
-import { BadRequestError, UnauthorizedError } from '../lib/errors';
+import { BadRequestError, UnauthorizedError } from '../../lib/errors';
 import { createResponseFromError } from 'cf-workers-utils';
+
+import logError from './logError';
 
 export default function errorHandler(error: Error): Response {
   // The full error message is "[@octokit/webhooks] signature does not match event payload and secret"
   // This will break if the message changes
   if (error instanceof AggregateError && error.message.includes('signature')) {
+    logError('verifyAndReceive', error);
+
     return createResponseFromError(
       new UnauthorizedError('Required crediential(s) is/are missing'),
       401
     );
   }
+
+  logError('fetch', error);
 
   const status = error instanceof BadRequestError ? 400 : 500;
 
