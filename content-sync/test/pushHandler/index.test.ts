073bd8accda548e7d5278c9acad346890fb951e6
Kynson Szetau <46522440+Kynson@users.noreply.github.com>
2025-01-08 17:25:54 +0800

    Update eslint config and pushHandler test types

--- content-sync/test/pushHandler/index.test.ts
+++ content-sync/test/pushHandler/index.test.ts
@@ -3,15 +3,29 @@ import pushHandler from '../../src/handlers/pushHandler';
 import { test, expect } from 'vitest';
 import { env } from 'cloudflare:test';
 
+
 test('pushHandler should fetch and put the parsed about into KV', async () => {
   // The follow is sufficient for the pushHandler
   const pushEvent = {
     repository: {
       full_name: 'Kynson/Kynson',
     },
-  } as any;
+  };
 
-  await pushHandler(pushEvent, env);
+  await pushHandler(pushEvent as any, env);
 
   await expect(env.CONTENT.get('about')).resolves.toBeTruthy();
+});
+
+test('pushHandler should not act upon repository except Kynson/Kynson', async () => {
+  // The follow is sufficient for the pushHandler
+  const pushEvent = {
+    repository: {
+      full_name: `Kynson/Kynson-${Math.random()}`,
+    },
+  };
+
+  await pushHandler(pushEvent as any, env);
+
+  await expect(env.CONTENT.get('about')).resolves.toBe(null);
 });
