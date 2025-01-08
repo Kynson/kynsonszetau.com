5e430d369669525f2741cea65c6dc6cbf84216b2
Kynson Szetau <46522440+Kynson@users.noreply.github.com>
2025-01-08 17:25:54 +0800

    Update eslint config and pushHandler test types

--- content-sync/eslint.config.js
+++ content-sync/eslint.config.js
@@ -1,3 +1,18 @@
 import { generateTSESLintConfigurations } from '@kynsonszetau/lint';
 
-export default generateTSESLintConfigurations(['src/**/*.ts']);
+/** @type { import('eslint').Linter.Config } */
+export default [
+  ...generateTSESLintConfigurations(
+    ['{src,test}/**/*.ts'],
+  ),
+  {
+    files: ['test/**/*.ts'],
+    rules: {
+      '@typescript-eslint/no-empty-object-type': {
+        allowInterfaces: 'with-single-extends',
+      },
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+    }
+  }
+];
