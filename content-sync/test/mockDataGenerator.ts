5d5cbaef868ef28df43206c0bc4f41c558d51ad4
Kynson Szetau <46522440+Kynson@users.noreply.github.com>
2025-01-11 16:52:18 +0800

    Update mock data generator utilities

--- content-sync/test/mockDataGenerator.ts
+++ content-sync/test/mockDataGenerator.ts
@@ -1,4 +1,5 @@
 import type { EmitterWebhookEventName } from '@octokit/webhooks';
+import type { EmitterWebhookEventPayload } from '../src/lib/octokit';
 
 interface GenerateRepositoryOptions {
   owner?: string;
@@ -13,8 +14,8 @@ type RepositoryActions<T = EmitterWebhookEventName> =
   T extends `repository.${infer S}` ? S : never;
 type SupportedEventName = 'push' | `repository.${RepositoryActions}`;
 
-interface GenerateRepositoryWebhookEventOptions {
-  action: RepositoryActions;
+interface GenerateRepositoryWebhookEventOptions<T extends RepositoryActions> {
+  action: T;
   changesFrom?: string;
   repositoryOptions?: GenerateRepositoryOptions;
 }
@@ -27,6 +28,11 @@ interface GenerateWebhookEventRequestOptions {
   repositoryOptions?: GenerateRepositoryOptions;
 }
 
+type GenerateProjectEntryOptions = Pick<
+  GenerateRepositoryOptions,
+  'name' | 'description' | 'topics'
+>;
+
 import { Webhooks } from '@octokit/webhooks';
 import { env } from 'cloudflare:test';
 import { faker } from '@faker-js/faker';
@@ -37,7 +43,7 @@ const webhooks = new Webhooks({
   secret: env.WEBHOOK_SECRET,
 });
 
-function generateRepository({
+export function generateRepository({
   owner = faker.internet.username(),
   name = faker.lorem.slug(),
   isPrivate = faker.datatype.boolean(),
@@ -63,11 +69,41 @@ function generateWebhookEventWithRepository(
   };
 }
 
-function generateRepositoryWebhookEvent({
+/**
+ * Generates a fake event sender
+ * This function is not configable as none of the event handler depends on the sender
+ * @returns The generated sender
+ */
+function generateWebhookEventSender() {
+  return {
+    name: faker.person.fullName(),
+    email: faker.internet.email(),
+    login: faker.internet.username(),
+    id: faker.number.int(),
+    node_id: faker.string.alphanumeric(),
+    avatar_url: faker.internet.url(),
+    gravatar_id: faker.string.alphanumeric(),
+    url: faker.internet.url(),
+    html_url: faker.internet.url(),
+    followers_url: faker.internet.url(),
+    following_url: faker.internet.url(),
+    gists_url: faker.internet.url(),
+    starred_url: faker.internet.url(),
+    subscriptions_url: faker.internet.url(),
+    organizations_url: faker.internet.url(),
+    repos_url: faker.internet.url(),
+    events_url: faker.internet.url(),
+    received_events_url: faker.internet.url(),
+    type: 'User',
+    site_admin: faker.datatype.boolean(),
+  };
+}
+
+export function generateRepositoryWebhookEvent<T extends RepositoryActions>({
   action,
   changesFrom = '',
   repositoryOptions = {},
-}: GenerateRepositoryWebhookEventOptions) {
+}: GenerateRepositoryWebhookEventOptions<T>): EmitterWebhookEventPayload<`repository.${T}`> {
   return {
     action,
     ...generateWebhookEventWithRepository(repositoryOptions),
@@ -75,9 +111,10 @@ function generateRepositoryWebhookEvent({
       changes: { repository: { name: { from: changesFrom } } },
     }),
     ...(action === 'transferred' && {
-      changes: { owner: { from: changesFrom } },
+      changes: { owner: { from: { user: { login: changesFrom } } } },
     }),
-  };
+    sender: generateWebhookEventSender(),
+  } as EmitterWebhookEventPayload<`repository.${T}`>;
 }
 
 export async function generateWebhookEventRequest({
@@ -112,4 +149,21 @@ export async function generateWebhookEventRequest({
     ...(omit !== 'payload' && { body: payload }),
     method: 'POST',
   });
+}
+
+export function generateProjectEntry({
+  name = faker.lorem.slug(),
+  description = faker.lorem.sentence(),
+  topics = faker.helpers.multiple(() => faker.hacker.noun()),
+}: GenerateProjectEntryOptions = {}): [
+  string,
+  { description: string; topics: string[] }
+] {
+  return [
+    name,
+    {
+      description,
+      topics,
+    },
+  ];
 }
