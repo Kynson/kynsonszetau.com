d9d3b67fad12ad9d5dc0be6581905242b3346737
Kynson Szetau <46522440+Kynson@users.noreply.github.com>
2025-01-12 11:48:34 +0800

    [Skip CI] Merge configs

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import { generateTSESLintConfigurations } from '@kynsonszetau/lint';
+
+/** @type { import('eslint').Linter.Config } */
+export default [
+  ...generateTSESLintConfigurations(['{content-sync,site}/{src,test}/**/*.ts']),
+  {
+    files: ['content-sync/test/**/*.ts'],
+    rules: {
+      '@typescript-eslint/no-empty-object-type': [
+        'error',
+        {
+          allowInterfaces: 'with-single-extends',
+        },
+      ],
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  },
+];
