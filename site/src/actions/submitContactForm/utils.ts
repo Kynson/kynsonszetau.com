1583a9b0756ca33b8c4d880648064214739af8a0
Kynson Szetau <46522440+Kynson@users.noreply.github.com>
2025-01-16 09:18:28 +0800

    Fix lint errors and polish submitContactForm action

--- site/src/actions/submitContactForm/utils.ts
+++ site/src/actions/submitContactForm/utils.ts
@@ -3,22 +3,10 @@
  * @param input [Optional] The string to escaped
  * @returns The escaped string
  */
-export function escape(input: string = ''): string {
+export function escape(input = ''): string {
   const specialCharactersRegex = /[_*~/`>|]/gm;
 
   return input.replace(/\\/g, '[&#92]').replace(specialCharactersRegex, '\\$&');
-}
-
-/**
- * Logs error to the console
- * @param action The action/ function that the error occurred at
- * @param error The error occurred
- */
-export function logError(action: string, error: unknown) {
-  console.error({
-    action,
-    error: error?.toString?.() || 'unknown',
-  });
 }
 
 /**
@@ -26,7 +14,7 @@ export function logError(action: string, error: unknown) {
  * @param endpoint The endpoint which the request will be sent to
  * @param rawBody The unencoded body to be attached in the request
  */
-export function postJSON(endpoint: string, rawBody: any) {
+export function postJSON(endpoint: string, rawBody: unknown) {
   return fetch(endpoint, {
     body: JSON.stringify(rawBody),
     method: 'POST',
