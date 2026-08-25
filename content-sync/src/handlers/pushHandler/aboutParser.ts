0acc6ebceb03d28c911b74645a16a92c5780f7ed
Kynson Szetau <46522440+Kynson@users.noreply.github.com>
2026-08-25 12:53:15 +0800

    feat(content-sync): Use JSON instead of parsing MD files

--- content-sync/src/handlers/pushHandler/aboutParser.ts
+++ content-sync/src/handlers/pushHandler/aboutParser.ts
@@ -1,133 +1,30 @@
-// class AboutElementRewriter {
-//   element(aboutElement: Element) {
-//     aboutElement.removeAndKeepContent();
-//   }
-// }
-
-// class GeneralElementRewriter {
-//   element(element: Element) {
-//     if (!element.removed) {
-//       element.remove();
-//     }
-//   }
-// }
-
-// class DocumentRewriter {
-//   comments(comment: Comment) {
-//     if (!comment.removed) {
-//       comment.remove();
-//     }
-//   }
-// }
-
-// const htmlRewriter = new HTMLRewriter()
-//   .on('about', new AboutElementRewriter())
-//   .on('*', new GeneralElementRewriter())
-//   .onDocument(new DocumentRewriter());
-
-interface Language {
-  displayName: string;
-  iconSlug: string;
-  iconColor: string;
-  iconColorBrightness: number;
-}
-
 import computeColorBrightness from './color';
-
-class AboutParser {
-  #introduction = '';
-  #languages: Language[] = [];
-
-  introductionElementHandler: HTMLRewriterElementContentHandlers = {
-    element(introductionElement) {
-      // Remove the element from the final result
-      introductionElement.remove();
-    },
-
-    text: ({ text }) => {
-      this.#introduction += text;
-    },
-  };
-
-  languageBadgeElementHandler: HTMLRewriterElementContentHandlers = {
-    element: (languageBadgeElement) => {
-      const displayName = languageBadgeElement.getAttribute('display-name');
-      const iconSlug = languageBadgeElement.getAttribute('icon-slug');
-      const iconColor = languageBadgeElement.getAttribute('icon-color');
-
-      if (!iconSlug || !displayName || !iconColor) {
-        throw new Error('Language badge element is malformed');
-      }
-
-      const iconColorBrightness = computeColorBrightness(iconColor);
-
-      this.#languages.push({
-        iconSlug,
-        displayName,
-        iconColor,
-        iconColorBrightness,
-      });
-
-      // Remove the element from the final result
-      languageBadgeElement.remove();
-    },
-  };
-
-  generalElementHandler: HTMLRewriterElementContentHandlers = {
-    element(element) {
-      if (element.removed) {
-        return;
-      }
-
-      element.remove();
-    },
-  };
-
-  documentHandler: HTMLRewriterDocumentContentHandlers = {
-    text(chunk) {
-      if (chunk.removed) {
-        return;
-      }
-
-      chunk.remove();
-    },
-
-    comments(comment) {
-      if (comment.removed) {
-        return;
-      }
-
-      comment.remove();
-    },
-
-    end: (documentEnd) => {
-      documentEnd.append(
-        JSON.stringify({
-          introduction: this.#introduction.trim(),
-          languages: this.#languages,
-        })
-      );
-    },
-  };
-}
+import { remoteAboutContentSchema } from 'common';
 
 /**
- * Parses the rawAboutResponse by removing all the unnecessary elements, and extract the content of the about element
- * @param rawAboutResponse The raw HTML of the readme page fetched from GitHub
+ * Parses the rawAboutResponse by first parsing the JSON response and then inject necessary keys into the JSON object
+ * @param rawAboutResponse The raw JSON response fetched from Github
  */
 async function parseAboutResponse(rawAboutResponse: Response): Promise<string> {
-  const aboutParser = new AboutParser();
-  const htmlRewriter = new HTMLRewriter()
-    .on('introduction', aboutParser.introductionElementHandler)
-    .on('img.language-badge', aboutParser.languageBadgeElementHandler)
-    .on('*', aboutParser.generalElementHandler)
-    .onDocument(aboutParser.documentHandler);
+  if (rawAboutResponse.status !== 200 && rawAboutResponse.status !== 304) {
+    throw new Error(`Unexpected response status: ${rawAboutResponse.status}`);
+  }
 
-  const parsedResponse = htmlRewriter.transform(rawAboutResponse);
-
-  // Get the text from the response and remove whitespace and newlines
   // Errors should be handled outside of this function
-  return (await parsedResponse.text()).trim();
+  const { introduction, languages } = remoteAboutContentSchema.parse(
+    await rawAboutResponse.json()
+  );
+
+  const aboutContent = {
+    introduction,
+    languages: languages.map((language) => ({
+      ...language,
+      iconColor: `#${language.iconColor}`,
+      iconColorBrightness: computeColorBrightness(language.iconColor)
+    }))
+  };
+
+  return JSON.stringify(aboutContent);
 }
 
 export default parseAboutResponse;
--- content-sync/test/pushHandler/aboutParser.test.ts
+++ content-sync/test/pushHandler/aboutParser.test.ts
@@ -10,37 +10,40 @@ test('parseAboutResponse should extracts the introduction and languages correctl
   const rustDisplayName = faker.internet.displayName();
 
   const response = new Response(
-    `
-			## Hello
-			<introduction>
-			${introduction}
-			</introduction>
-			# An image
-			<img src="http://example.com"/>
-      <!-- This is a comment -->
-			# Language
-			<img class="language-badge" src="http://example.com" display-name="${javascriptDisplayName}" icon-slug="js" icon-color="#e8d502">
-			<img class="language-badge" src="http://example.com" display-name="${rustDisplayName}" icon-slug="rust" icon-color="#f46623">
-			`
-  );
-
-  await expect(parseAboutResponse(response)).resolves.toBe(
     JSON.stringify({
       introduction,
       languages: [
         {
           iconSlug: 'js',
           displayName: javascriptDisplayName,
+          iconColor: 'e8d502'
+        },
+        {
+          iconSlug: 'rust',
+          displayName: rustDisplayName,
+          iconColor: 'f46623'
+        }
+      ]
+    })
+  );
+
+  await expect(parseAboutResponse(response).then((result) => JSON.parse(result))).resolves.toEqual(
+    {
+      introduction,
+      languages: [
+        {
+          iconSlug: 'js',
+          displayName: javascriptDisplayName,
           iconColor: '#e8d502',
-          iconColorBrightness: 0.8255981020247668,
+          iconColorBrightness: 0.8255981020247668
         },
         {
           iconSlug: 'rust',
           displayName: rustDisplayName,
           iconColor: '#f46623',
-          iconColorBrightness: 0.5766258294372216,
-        },
-      ],
-    })
+          iconColorBrightness: 0.5766258294372216
+        }
+      ]
+    }
   );
 });
